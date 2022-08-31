f0e4da3fa34e44a34b4a0f606e2ad7a41b14e1b9
RL20 <harel_shalom@yahoo.com>
2022-08-31 14:35:49 +0300

    fix post type

--- wp-content/themes/twenty-twenty-child/functions.php
+++ wp-content/themes/twenty-twenty-child/functions.php
@@ -6,15 +6,13 @@
 	   wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 	}
 
-//●	Disable wp admin bar for this user, using code
 add_action('after_setup_theme', 'remove_admin_bar');
 function remove_admin_bar() {
 if (!current_user_can('administrator') && !is_admin()) {
   show_admin_bar(false);
 }
 
-// Our custom post type function
-function create_posttype() {
+function QuadLayers_custom_post_type() {
 	$labels = array(
 		'name'                => _x( 'Products', 'Post Type General Name', 'twentytwenty' ),
 		'singular_name'       => _x( 'Product', 'Post Type Singular Name', 'twentytwenty' ),
@@ -32,26 +30,28 @@ function create_posttype() {
 );
 	
   
-	register_post_type( 'products',
-	// CPT Options
-	 array(
-					'labels' => $labels,
-					'main_image' => true,
-					'product_image_gallery ' => array('','','','','',''),
-					'title' => '',
-					'description'=>'',
-					'price'=>'',
-					'sale_price'=>'',
-					'is_on_sale'=>'',
-					'youtube_video'=>'',
-					'category'=>'',
-
-			)
-	);
+$args = array(
+	'label'               => __( 'movies', 'storefront' ),
+	'description'         => __( 'Movie news and reviews', 'storefront' ),
+	'labels'              => $labels,  
+	'supports'            => array( 'title', 'editor', 'excerpt', 'author', 'thumbnail', 'comments', 'revisions', 'custom-fields', ),     
+	'taxonomies'          => array( 'genres' ),     
+	'hierarchical'        => false,
+	'public'              => true,
+	'show_ui'             => true,
+	'show_in_menu'        => true,
+	'show_in_nav_menus'   => true,
+	'show_in_admin_bar'   => true,
+	'menu_position'       => 5,
+	'can_export'          => true,
+	'has_archive'         => true,
+	'exclude_from_search' => false,
+	'publicly_queryable'  => true,
+	'capability_type'     => 'post',
+	'show_in_rest' => true, 
+);
+// Registering your Custom Post Type
+register_post_type( 'products', $args );
 }
-// Hooking up our function to theme setup
-add_action( 'init', 'create_posttype' );
-
-
-
+add_action( 'init', 'QuadLayers_custom_post_type', 0 );
 }?>
